9b30e276ad93c7a1f0c7af3597c7b9dad5736c4e
Bkim057 <kim3224@purdue.edu>
2022-10-25 13:32:02 -0400

    Fix to models... oops

--- models.py
+++ models.py
@@ -57,6 +57,7 @@ class User(UserMixin, db.Model):
     chat_restriction = db.Column(db.Boolean)
     pfp_filename = db.Column(db.String(50))
     pfp = db.Column(LargeBinary)
+    admin = db.Column(db.Boolean)
     # End of warning
     followed_topics = db.relationship('Topic', secondary=user_topic, backref='followed_by', lazy='dynamic')
     comments = db.relationship('Commented',  backref='user', passive_deletes=True)
